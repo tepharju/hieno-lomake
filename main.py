ce6ed09ada27b19cb337b912030013ad10828bc9
Teppo Harju <Teppo.Harju@mayk.fi>
2025-03-24 14:55:46 +0200

    Lisätty uusi lempinimigeneraattori

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import random
 
 app = Flask(__name__)
 
@@ -9,9 +10,15 @@ def root():
 @app.route("/vastaus")
 def vastaus():
     uusi_nimi = request.args["nimi"]
-    uusi_nimi = uusi_nimi * 2
+    uusi_nimi = uusi_nimi 
     
     lempinimi = request.args["lempinimi"]
+    
+    loppuliitteet = ["-ski", "-ster", "-y", "-o", "-man"]
+    
+    liite = random.choice(loppuliitteet)
+    
+    uusi_lempinimi = uusi_nimi + liite
     
     #with open("kaikki_nimet.txt", "a") as nimitiedosto:
     #    nimitiedosto.write(uusi_nimi + "\n")
@@ -19,7 +26,7 @@ def vastaus():
     #kaikki_nimet = open("kaikki_nimet.txt").read()
     #return render_template('vastaus.html', nimi=kaikki_nimet)
     #return render_template('vastaus.html', nimi=request.args['nimi'])
-    return render_template('vastaus.html', nimi=uusi_nimi, lempinimi=lempinimi)
+    return render_template('vastaus.html', nimi=uusi_nimi, lempinimi=uusi_lempinimi)
 
 if __name__ == '__main__':
     app.run()
